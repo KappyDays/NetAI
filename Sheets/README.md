faa8b3f480f640c70c8027328e38076c20e5aad4
KappyDays <rkdfuf2wkd@naver.com>
2024-08-26 17:33:13 +0900

    Add Google Sheets API example

---
+++ Sheets/README.md
@@ -0,0 +1,9 @@
+1. Google Cloud Platform (GCP) 접속
+   
+    https://cloud.google.com/free/?utm_source=google&utm_medium=cpc&utm_campaign=japac-KR-all-en-dr-BKWS-all-core-trial-EXA-dr-1605216&utm_content=text-ad-none-none-DEV_c-CRE_644033776725-ADGP_Hybrid+%7C+BKWS+-+EXA+%7C+Txt+-GCP-General-core+brand-main-KWID_43700074755444104-kwd-6458750523&userloc_1009856-network_g&utm_term=KW_google+cloud&gad_source=1&gclid=Cj0KCQjwz7C2BhDkARIsAA_SZKY2ZYjVwlwPRSAwkzkB6woldX2lXhsAQSWul4zuJRzZDgnsuX7VftUaAovMEALw_wcB&gclsrc=aw.ds&hl=ko
+2. 우측 상단의 **콘솔** 접속
+3. 메인 화면 > 제품 > API (서비스 액세스) 접속
+4. sheet 검색 > Google Sheets API 클릭 > 사용 클릭
+5. 사이드바 > IAM 및 관리자 > 서비스 계정 > 서비스 계정 만들기
+6. 생성된 서비스 계정에 접속 > 키 > 키 추가 > json 파일 저장
+7. Google Spread Sheets 생성 > 공유 > 서비스 계정 이메일 추가
---
+++ Sheets/sheets_integration.py
@@ -0,0 +1,39 @@
+from pprint import pprint
+from googleapiclient import discovery
+from google.oauth2 import service_account
+import json
+
+def json_read(json_file):
+    with open(json_file, 'r') as f:
+        return json.load(f)
+    
+class GooglesheetUtils:
+    def __init__(self) -> None:
+        # sheet id는 구글 스프레드 시트의 url에서 확인 가능
+        # ex) ~/spreadsheets/d/sheet_id/~
+        sheetID_json = json_read('my_private_setting.json')
+        self.spreadsheet_id = sheetID_json["sheet_id"]
+        self.credentials = service_account.Credentials.from_service_account_file(
+            # Google Cloud > 서비스 계정 > 서비스 계정 만들기 > 키 > 키 추가 > json 파일 다운로드
+            'ultra-optics-433707-s2-5e5e3a11e245.json',
+            scopes = ['https://www.googleapis.com/auth/spreadsheets']
+        )
+        self.service = discovery.build('sheets','v4', credentials=self.credentials)
+
+    def read_spreadsheet(self, range_name) -> None:
+        service = discovery.build('sheets','v4', credentials=self.credentials)
+        result = service.spreadsheets().values().get(spreadsheetId=self.spreadsheet_id, range=range_name).execute()
+    
+        # Get the values from the response
+        values = result.get('values', [])
+
+        # Print the values
+        for row in values:
+            print(row)
+            
+def main():
+    googlesheetUtils = GooglesheetUtils()
+    googlesheetUtils.read_spreadsheet('DataCenter!A1:B8')
+
+if __name__ == "__main__":
+    main()            
